a735b1046d6c24f52ee02d6be27bec36021075fc
suricactus <suricactus@orange>
2013-02-16 10:07:27 +0200

    Huge improvements Almost all the code is rewrote from scratch

--- files/js/application/map.js
+++ files/js/application/map.js
@@ -13,6 +13,7 @@ function Map(settings) {
     var _unitToAdd = null;
     var _unitToAddType = null;
     var _cellDimensions = (settings.dimensions - 2 * settings.offset) / settings.size;
+    var _halfDimensions = settings.dimensions / 2;
     
     // Private methods
     /**
@@ -35,7 +36,57 @@ function Map(settings) {
         });
         layer.add(background);
     }
-    
+    function updateNavigationGradientSize(shape, offsetX, offsetY) {
+        var flagX = false;
+        var flagY = false;
+        var leftEdge = (_halfDimensions > offsetX);
+        var topEdge = (_halfDimensions > offsetY);
+        var edgeDistanceX = _halfDimensions - Math.abs(_halfDimensions - offsetX) - ((leftEdge) ? 0 : 1);
+        var edgeDistanceY = _halfDimensions - Math.abs(_halfDimensions - offsetY) - ((topEdge) ? 0 : 1);
+        var circleX = offsetX;
+        var circleY = offsetY;
+        var radius;
+        var radiusByX;
+        var radiusByY;
+
+        if (edgeDistanceX < edgeDistanceY && edgeDistanceX < settings.offsetNavigation) {
+            flagX = true;
+            circleX = -edgeDistanceX - 2.5 * settings.offsetNavigation;
+            radiusByX = 2 * ((settings.offsetNavigation - edgeDistanceX) + settings.offsetNavigation);
+            radius = radiusByX;
+
+            // If it's closer to right edge, add the width of battlefield and change the sign
+            if (!leftEdge) {
+                circleX = settings.dimensions - circleX;
+            }
+        }
+        if (edgeDistanceY < edgeDistanceX && edgeDistanceY < settings.offsetNavigation) {
+            flagY = true;
+            circleY = -edgeDistanceY - 2.5 * settings.offsetNavigation;
+            radiusByY = 2 * ((settings.offsetNavigation - edgeDistanceY) + settings.offsetNavigation);
+            radius = (radiusByX) ? Math.max(radiusByX, radiusByY) : radiusByY;
+
+            // If it's closer to bottom edge, add the width of battlefield and change the sign
+            if (!topEdge) {
+                circleY = settings.dimensions - circleY;
+            }
+        }
+
+        if (flagX || flagY) {
+            if (settings.navigationGradientEnabled) {
+                shape.setAttrs({
+                    x: circleX,
+                    y: circleY,
+                    radius: radius,
+                    fillRadialGradientEndRadius: radius,
+                    visible: true
+                });
+                shape.parent.draw();
+            }
+        } else {
+            settings.navigationGradientEnabled = true;
+        }
+    }
     function buildOverlay(layer) {
         var circle = new Kinetic.Circle({
             fillRadialGradientStartRadius: 0,
@@ -43,131 +94,78 @@ function Map(settings) {
             fillRadialGradientEndPoint: [0, 0],
             fillRadialGradientColorStops: [0.6, 'black', 1, 'transparent'],
             fillRadialGradientEndRadius: 1
-        });
-        layer.on('mousemove', function(e) {
-            var flagX = false;
-            var flagY = false;
-            var halfDimensions = settings.dimensions / 2;
-            var leftEdge = (halfDimensions > e.offsetX);
-            var topEdge = (halfDimensions > e.offsetY);
-            var edgeDistanceX = halfDimensions - Math.abs(halfDimensions - e.offsetX) - ((leftEdge) ? 0 : 1);
-            var edgeDistanceY = halfDimensions - Math.abs(halfDimensions - e.offsetY) - ((topEdge) ? 0 : 1);
-            var circleX = e.offsetX;
-            var circleY = e.offsetY;
-            var radius;
-            var radiusByX;
-            var radiusByY;
-            
-            if (edgeDistanceX < edgeDistanceY && edgeDistanceX < settings.offsetNavigation) {
-                flagX = true;
-                circleX = -edgeDistanceX - 2.5 * settings.offsetNavigation;
-                radiusByX = 2 * ((settings.offsetNavigation - edgeDistanceX) + settings.offsetNavigation);
-                radius = radiusByX;
-                
-                // If it's closer to right edge, add the width of battlefield and change the sign
-                if(!leftEdge) {
-                    circleX = settings.dimensions - circleX;
-                }
-            }
-            if (edgeDistanceY < edgeDistanceX &&  edgeDistanceY < settings.offsetNavigation) {
-                flagY = true;
-                circleY = -edgeDistanceY - 2.5 * settings.offsetNavigation;
-                radiusByY = 2 * ((settings.offsetNavigation - edgeDistanceY) + settings.offsetNavigation);
-                radius = (radiusByX) ? Math.max(radiusByX, radiusByY) : radiusByY;
-                
-                // If it's closer to bottom edge, add the width of battlefield and change the sign
-                if(!topEdge) {
-                    circleY = settings.dimensions - circleY;
-                }
-            }
-            
-            if (flagX || flagY) {
-                circle.setAttrs({
-                    x: circleX, 
-                    y: circleY,
-                    radius: radius,
-                    fillRadialGradientEndRadius: radius,
-                    visible: true
-                });
-            }
-            layer.draw();
-        });
+        }); 
         layer.add(circle);
-        layer.on('mouseleave', function(e) {
-            $(document).on('click.app', function(event) {
+        
+        layer.on('mousemove.navigationGradient', function(e) {
+            updateNavigationGradientSize(circle, e.offsetX, e.offsetY);
+        });
+        layer.on('mouseleave.navigationGradient', function(e) {
+            var leftEdge = (_halfDimensions > e.offsetX);
+            var topEdge = (_halfDimensions > e.offsetY);
+            var x = e.offsetX;
+            var y = e.offsetY;
+            if (leftEdge && topEdge) {
+                if(x < y) {
+                    x = 0;
+                } else {
+                    y = 0;
+                }
+            } else if(leftEdge){
+                if(x < settings.dimensions - y) {
+                    x = 0;
+                } else {
+                    y = settings.dimensions;
+                }
+            } else if(topEdge) {                
+                if(settings.dimensions - x < y) {
+                    x = settings.dimensions;
+                } else {
+                    y = 0;
+                }
+            } else {
+                if(x > y) {
+                    x = settings.dimensions;
+                } else {
+                    y = settings.dimensions;
+                }
+            }
+
+            updateNavigationGradientSize(circle, x, y);
+                    
+            $(document).on('click.navigationGradient', function(event) {
                 circle.hide();
                 layer.draw();
-                $(document).off('click.app')
+                settings.navigationGradientEnabled = false;
+                $(document).off('click.navigationGradient')
             });
         });
     }
 
-    function buildUnit(layer, type) {
-        // Private properties
-        var _unitProperties = app.core.getUnitProperties(type);
-        var _range;
-        var _unit;
-        
-        // Private methods
-        function getRandomUnitImage(type) {
-            var image = new Image();
-            var randomImage = _unitProperties.images[Math.floor(Math.random()* (_unitProperties.images.length))];
-            image.src = CONST.images[randomImage];
-            return image;
-        }
-        function getGriddedMapPosition(position) {
-            return Math.round((position - settings.offset) / _cellDimensions);
-        }
-        
-        // Privileged methods
-        
-        // Constructor
-        var _range = new Kinetic.Circle({
-//            radius: range * _cellDimensions,
-            radius: 2 * _cellDimensions,
-            stroke: 'black',
-            strokeWidth: 2,
-            visible: false
+    function handlerMapMousemoveUnitPlacing(e) {
+        _unitToAdd.setAttrs({
+            x: e.offsetX - _cellDimensions / 2,
+            y: e.offsetY - _cellDimensions / 2,
+            visible: true
         });
-        var _unit = new Kinetic.Image({
-            visible: false,
-            image: getRandomUnitImage(type),
-            width: _cellDimensions,
-            height: _cellDimensions,
-            draggable: true
-        });
-        
-        layer.on('mousemove.buildUnit', function(e) {
-            _unit.setAttrs({
-                x: e.offsetX - _cellDimensions / 2,
-                y: e.offsetY - _cellDimensions / 2,
-                visible: true
-            });
-            _range.setAbsolutePosition(e.offsetX, e.offsetY);
-            _range.show();
-            layer.draw();
-        });
-        layer.on('mouseleave.buildUnit', function(e) {
-            _unit.hide();
-            _range.hide();
-            layer.draw();
-        });
-        layer.on('click.buildUnit', function(e) {
-            var mapX = getGriddedMapPosition(e.offsetX);
-            var mapY = getGriddedMapPosition(e.offsetY);
-            console.log(mapX, mapY)
-//            unit.setAbsolutePosition(mapX * _cellDimensions, mapY * _cellDimensions);
-            _range.remove();
-            layer.off('mousemove.buildUnit click.buildUnit');
-            layer.draw();
-        });
-        
-        layer
-            .add(_range)
-            .add(_unit)
-            .draw();
-        return _unit;
+        _layerMap.draw();
     }
+    function handlerMapMouseleaveUnitPlacing(e) {
+        _unitToAdd.hide();
+        _layerMap.draw();
+    }
+//    function getGriddedMapPosition(position) {
+//        return Math.round((position - settings.offset) / _cellDimensions);
+//    }
+//    layer.on('click.buildUnit', function(e) {
+//        var mapX = getGriddedMapPosition(e.offsetX);
+//        var mapY = getGriddedMapPosition(e.offsetY);
+//        console.log(mapX, mapY)
+////            unit.setAbsolutePosition(mapX * _cellDimensions, mapY * _cellDimensions);
+//        _range.remove();
+//        layer.off('mousemove.buildUnit click.buildUnit');
+//        layer.draw();
+//    });
 
     // Privileged methods
     this.getSettings = function() {
@@ -179,30 +177,49 @@ function Map(settings) {
     this.getMap = function() {
         return _layerMap;
     };
+    this.getCellDimensions= function() {
+        return _cellDimensions;
+    };
 
     // Constructor
+    settings.navigationGradientEnabled = false;
+    
+    // On unit change
     $(document).bind('unitChange.app', function (e, type) {
+        // If there is selected type
         if (type) {
-            if (type !== _unitToAddType) {
-                if (_unitToAdd) {
-                    _unitToAdd.remove();
-                }
-                _unitToAddType = type;
-                _unitToAdd = buildUnit(_layerMap, type);
+            var flag = false;
+            // If the new unit would different from previous one
+            if (_unitToAdd && type !== _unitToAdd.getType()) {
+                _unitToAdd.destroy();
+                flag = true;
+            // If there wasn't previous one
+            } else if (!_unitToAdd) {
+                flag = true;
+            }
+            
+            if (flag) {
+                _unitToAdd = app.core.createNewUnit(type, {
+                    dimensions: _cellDimensions,
+                    quantity: app.market.getQuantity(),
+                    layer: _layerMap
+                });
                 _unitToAdd.show();
+                
+                _layerMap.on('mousemove.unitPlacing', handlerMapMousemoveUnitPlacing);
+                _layerMap.on('mouseleave.unitPlacing', handlerMapMouseleaveUnitPlacing);
             }
         } else {
-            var layer = _unitToAdd.parent;
-            _unitToAdd.remove();
+            _unitToAdd.destroy();
             _unitToAdd = null;
             _unitToAddType = null;
-            layer.draw();
+            _layerMap.draw();
+            _layerMap.off('.unitPlacing');
         }
     });
     
     buildBackground(_layerMap);
     buildOverlay(_layerMap);
-
 
     _stage.add(_layerMap);
     _stage.add(_layerOverlay);
